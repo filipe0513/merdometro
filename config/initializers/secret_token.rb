fff96d94016b31e014b1b5698d717bb56b565fe3
Filipe Magalhaes <filipe0513@gmail.com>
2013-07-07 23:02:24 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merdometro::Application.config.secret_token = 'b84246d985f3e0e9393f207333bd969ae2823c1b5e0c40d535ec8de0db876dbb810cb885ff11dd68939da0dd26b17e66a838082bd9c8ef857c6f974b82599ae6'
